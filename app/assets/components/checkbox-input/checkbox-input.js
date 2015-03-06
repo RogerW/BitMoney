d2b72d72b25291876e39564f4bb8a6c09bdff02c
Roger_Wilco <stsvorobev@gmail.com>
2015-03-06 11:21:35 +0000

    Sign In Form so so

---
+++ app/assets/components/checkbox-input/checkbox-input.js
@@ -0,0 +1,21 @@
+Polymer('checkbox-input', {
+    publish: {
+        label:'checkbox',
+        input_name:'checkbox'
+    }, 
+    
+    change : function(e, detail, sender){
+        var nodes = this.$.content.getDistributedNodes();
+        for (var i = 0; i < nodes.length; i++) {
+            if(nodes[i].getAttribute("type", "checkbox")) {
+                if(sender.getAttribute("aria-checked") == 'true') {
+                    nodes[i].checked = false;
+                } else {
+                    nodes[i].checked = true;
+                }
+            }
+            
+        }
+
+    }
+});
